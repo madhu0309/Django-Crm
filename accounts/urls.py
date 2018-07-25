b16e336007e846831d4dce08210197d4c797ab4d
Shirisha Gaddi <sirisha.gaddi@micropyramid.com>
2018-07-25 17:48:35 +0530

    Updated accounts views to cbv

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,19 +1,21 @@
 from django.conf.urls import url
 from django.urls import path
 from accounts import views
-from accounts.views import AccountsListView, CreateAccountView
+from accounts.views import (
+	AccountsListView, CreateAccountView, AccountDetailView, AccountUpdateView,
+	AccountDeleteView, AddCommentView)
 
 app_name = 'accounts'
 
 urlpatterns = [
 	path('list/', AccountsListView.as_view(), name='list'),
 	path('create/', CreateAccountView.as_view(), name='new_account'),
+	path('<int:pk>/view/', AccountDetailView.as_view(), name="view_account"),
+	path('<int:pk>/edit/', AccountUpdateView.as_view(), name="edit_account"),
+	path('<int:pk>/delete/', AccountDeleteView.as_view(), name="remove_account"),
+	path('comment/add/', AddCommentView.as_view(), name="add_comment"),
 
-    url(r'^(?P<account_id>\d*)/view/$', views.view_account, name="view_account"),
-    url(r'^(?P<edid>\d*)/edit/$', views.edit_account, name="edit_account"),
-    url(r'^(?P<aid>\d*)/delete/$', views.remove_account, name="remove_account"),
-
-    url(r'^comment/add/$', views.add_comment, name='add_comment'),
+    # url(r'^comment/add/$', views.add_comment, name='add_comment'),
     url(r'^comment/edit/$', views.edit_comment, name='edit_comment'),
     url(r'^comment/remove/$', views.remove_comment, name='remove_comment'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,9 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import CreateView, UpdateView, DetailView, ListView, TemplateView, View
+from django.urls import reverse
+from django.views.generic import (
+    CreateView, UpdateView, DetailView, ListView, TemplateView, View, DeleteView)
 
 from accounts.models import Account
 from common.models import User, Address, Team, Comment
@@ -122,122 +124,157 @@ class CreateAccountView(LoginRequiredMixin, CreateView):
         return context
 
 
-@login_required
-def view_account(request, account_id):
-    account_record = get_object_or_404(Account, id=account_id)
-    comments = account_record.accounts_comments.all()
-    opportunity_list = Opportunity.objects.filter(account=account_record)
-    contacts = Contact.objects.filter(account=account_record)
-    users = User.objects.filter(is_active=True).order_by('email')
-    cases = Case.objects.filter(account=account_record)
-    teams = Team.objects.all()
-    return render(request, "view_account.html", {
-        'account_record': account_record,
-        'opportunity_list': opportunity_list,
-        'stages': STAGES,
-        'sources': SOURCES,
-        'teams': teams,
-        'contacts': contacts,
-        'users': users,
-        'cases': cases,
-        'countries': COUNTRIES,
-        'currencies': CURRENCY_CODES,
-        'case_types': CASE_TYPE,
-        'case_priority': PRIORITY_CHOICE,
-        'case_status': STATUS_CHOICE,
-        'comments': comments
-    })
+class AccountDetailView(LoginRequiredMixin, DetailView):
+    model = Account
+    context_object_name = "account_record"
+    template_name = "view_account.html"
 
-
-@login_required
-def edit_account(request, edid):
-    account_instance = get_object_or_404(Account, id=edid)
-    account_billing_address = account_instance.billing_address
-    account_shipping_address = account_instance.shipping_address
-    users = User.objects.filter(is_active=True).order_by('email')
-    account_form = AccountForm(instance=account_instance, assigned_to=users)
-    billing_form = BillingAddressForm(instance=account_billing_address)
-    shipping_form = ShippingAddressForm(prefix='ship', instance=account_shipping_address)
-    teams = Team.objects.all()
-    assignedto_list = request.POST.getlist('assigned_to')
-    teams_list = request.POST.getlist('teams')
-    if request.method == 'POST':
-        account_form = AccountForm(request.POST, instance=account_instance, assigned_to=users)
-        billing_form = BillingAddressForm(request.POST, instance=account_billing_address)
-        shipping_form = ShippingAddressForm(request.POST, instance=account_shipping_address, prefix='ship')
-        if account_form.is_valid() and billing_form.is_valid() and shipping_form.is_valid():
-            billing_object = billing_form.save()
-            shipping_object = shipping_form.save()
-            account_object = account_form.save(commit=False)
-            account_object.billing_address = billing_object
-            account_object.shipping_address = shipping_object
-            account_object.created_by = request.user
-            account_object.save()
-            account_object.assigned_to.clear()
-            account_object.assigned_to.add(*assignedto_list)
-            account_object.teams.clear()
-            account_object.teams.add(*teams_list)
-            return redirect("accounts:list")
-        else:
-            return render(request, 'create_account.html', {
-                'account_form': account_form,
-                'billing_form': billing_form,
-                'shipping_form': shipping_form,
-                'account_obj': account_instance,
-                'billing_obj': account_billing_address,
-                'shipping_obj': account_shipping_address,
-                'countries': COUNTRIES,
-                'industries': INDCHOICES,
-                'teams': teams,
-                'users': users,
-                'assignedto_list': [int(user_id) for user_id in assignedto_list],
-                'teams_list': teams_list
-            })
-    else:
-        return render(request, 'create_account.html', {
-            'account_form': account_form,
-            'billing_form': billing_form,
-            'shipping_form': shipping_form,
-            'account_obj': account_instance,
-            'billing_obj': account_billing_address,
-            'shipping_obj': account_shipping_address,
-            'countries': COUNTRIES,
-            'industries': INDCHOICES,
-            'teams': teams,
-            'users': users,
-            'assignedto_list': assignedto_list,
-            'teams_list': teams_list
+    def get_context_data(self, **kwargs):
+        context = super(AccountDetailView, self).get_context_data(**kwargs)
+        account_record = context["account_record"]
+        context.update({
+            "comments": account_record.accounts_comments.all(),
+            "opportunity_list": Opportunity.objects.filter(account=account_record),
+            "contacts": Contact.objects.filter(account=account_record),
+            "users": User.objects.filter(is_active=True).order_by('email'),
+            "cases": Case.objects.filter(account=account_record),
+            "teams": Team.objects.all(),
+            "stages": STAGES,
+            "sources": SOURCES,
+            "countries": COUNTRIES,
+            "currencies": CURRENCY_CODES,
+            "case_types": CASE_TYPE,
+            "case_priority": PRIORITY_CHOICE,
+            "case_status": STATUS_CHOICE,
         })
+        return context
 
 
-@login_required
-def remove_account(request, aid):
-    account_record = get_object_or_404(Account, id=aid)
-    account_record.delete()
-    return redirect("accounts:list")
+class AccountUpdateView(LoginRequiredMixin, UpdateView):
+    model = Account
+    form_class = AccountForm
+    template_name = "create_account.html"
 
+    def dispatch(self, request, *args, **kwargs):
+        self.users = User.objects.filter(is_active=True).order_by('email')
+        return super(AccountUpdateView, self).dispatch(request, *args, **kwargs)
 
-@login_required
-def add_comment(request):
-    if request.method == 'POST':
-        account = get_object_or_404(Account, id=request.POST.get('accountid'))
-        if request.user in account.assigned_to.all() or request.user == account.created_by:
-            form = AccountCommentForm(request.POST)
-            if form.is_valid():
-                account_comment = form.save(commit=False)
-                account_comment.comment = request.POST.get('comment')
-                account_comment.commented_by = request.user
-                account_comment.account = account
-                account_comment.save()
-                data = {"comment_id": account_comment.id, "comment": account_comment.comment,
-                        "commented_on": account_comment.commented_on,
-                        "commented_by": account_comment.commented_by.email}
-                return JsonResponse(data)
+    def get_form_kwargs(self):
+        kwargs = super(AccountUpdateView, self).get_form_kwargs()
+        kwargs.update({'assigned_to': self.users})
+        return kwargs
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        billing_form = BillingAddressForm(request.POST, instance=self.object.billing_address)
+        shipping_form = ShippingAddressForm(
+            request.POST, instance=self.object.shipping_address, prefix='ship')
+        if form.is_valid() and billing_form.is_valid() and shipping_form.is_valid():
+            return self.form_valid(form, billing_form, shipping_form)
+        else:
+            return self.form_invalid(form, billing_form, shipping_form)
+
+    def form_valid(self, form, billing_form, shipping_form):
+        # Save Billing & Shipping Address
+        billing_address_object = billing_form.save()
+        shipping_address_object = shipping_form.save()
+        # Save Account
+        account_object = form.save(commit=False)
+        account_object.billing_address = billing_address_object
+        account_object.shipping_address = shipping_address_object
+        account_object.save()
+        account_object.assigned_to.clear()
+        account_object.teams.clear()
+        if self.request.POST.getlist('assigned_to', []):
+            account_object.assigned_to.add(*self.request.POST.getlist('assigned_to'))
+        if self.request.POST.getlist('teams', []):
+            account_object.teams.add(*self.request.POST.getlist('teams'))
+        return redirect("accounts:list")
+
+    def form_invalid(self, form, billing_form, shipping_form):
+        return self.render_to_response(
+            self.get_context_data(
+                form=form, billing_form=billing_form, shipping_form=shipping_form)
+        )
+
+    def get_context_data(self, **kwargs):
+        context = super(AccountUpdateView, self).get_context_data(**kwargs)
+        context["account_obj"] = self.object
+        context["billing_obj"] = self.object.billing_address
+        context["shipping_obj"] = self.object.shipping_address
+        context["account_form"] = context["form"]
+        context["users"] = self.users
+        context["industries"] = INDCHOICES
+        context["countries"] = COUNTRIES
+        context["teams"] = Team.objects.all()
+        if "billing_form" in kwargs and "shipping_form" in kwargs:
+            context["billing_form"] = kwargs["billing_form"]
+            context["shipping_form"] = kwargs["shipping_form"]
+        else:
+            if self.request.POST:
+                context["billing_form"] = BillingAddressForm(
+                    request.POST, instance=self.object.billing_address)
+                context["shipping_form"] = ShippingAddressForm(
+                    request.POST, instance=self.object.shipping_address, prefix='ship')
             else:
-                return JsonResponse({"error": form['comment'].errors})
+                context["billing_form"] = BillingAddressForm(
+                    instance=self.object.billing_address)
+                context["shipping_form"] = ShippingAddressForm(
+                    instance=self.object.shipping_address, prefix='ship')
+        context["assignedto_list"] = [
+            int(i) for i in self.request.POST.getlist('assigned_to', []) if i]
+        context["teams_list"] = [
+            int(i) for i in self.request.POST.getlist('teams', []) if i]
+        return context
+
+
+class AccountDeleteView(LoginRequiredMixin, DeleteView):
+    model = Account
+    template_name = 'view_account.html'
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.billing_address.delete()
+        self.object.shipping_address.delete()
+        self.object.delete()
+        return redirect("accounts:list")
+
+
+class AddCommentView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = AccountCommentForm
+    http_method_names = ["post"]
+
+    def post(self, request, *args, **kwargs):
+        self.object = None
+        self.account = get_object_or_404(Account, id=request.POST.get('accountid'))
+        if (
+            request.user in self.account.assigned_to.all() or
+            request.user == self.account.created_by
+        ):
+            form = self.get_form()
+            if form.is_valid():
+                return self.form_valid(form)
+            else:
+                return self.form_invalid(form)
         else:
             data = {'error': "You Dont Have permissions to Comment"}
             return JsonResponse(data)
+
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.commented_by = self.request.user
+        comment.account = self.account
+        comment.save()
+        return JsonResponse({
+            "comment_id": comment.id, "comment": comment.comment,
+            "commented_on": comment.commented_on,
+            "commented_by": comment.commented_by.email
+        })
+
+    def form_invalid(self, form):
+        return JsonResponse({"error": form['comment'].errors})
 
 
 @login_required
