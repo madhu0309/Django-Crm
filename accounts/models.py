de68a11121041d8a13943587836ba75d86692f98
Ashwin <ashwin@micropyramid.com>
2019-01-27 13:34:25 +0530

    sync with github

--- accounts/models.py
+++ accounts/models.py
@@ -12,19 +12,14 @@ class Account(models.Model):
         "Name of Account", "Name"), max_length=64)
     email = models.EmailField()
     phone = PhoneNumberField(null=True)
-    industry = models.CharField(
-        _("Industry Type"), max_length=255, choices=INDCHOICES, blank=True, null=True)
-    billing_address = models.ForeignKey(
-        Address, related_name='account_billing_address', on_delete=models.CASCADE, blank=True, null=True)
-    shipping_address = models.ForeignKey(
-        Address, related_name='account_shipping_address', on_delete=models.CASCADE, blank=True, null=True)
+    industry = models.CharField(_("Industry Type"), max_length=255, choices=INDCHOICES, blank=True, null=True)
+    billing_address = models.ForeignKey(Address, related_name='account_billing_address', on_delete=models.CASCADE, blank=True, null=True)
+    shipping_address = models.ForeignKey(Address, related_name='account_shipping_address', on_delete=models.CASCADE, blank=True, null=True)
     website = models.URLField(_("Website"), blank=True, null=True)
     description = models.TextField(blank=True, null=True)
-    assigned_to = models.ManyToManyField(
-        User, related_name='account_assigned_to')
+    assigned_to = models.ManyToManyField(User, related_name='account_assigned_to')
     teams = models.ManyToManyField(Team)
-    created_by = models.ForeignKey(
-        User, related_name='account_created_by', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='account_created_by', on_delete=models.CASCADE)
     created_on = models.DateTimeField(_("Created on"), auto_now_add=True)
     is_active = models.BooleanField(default=False)
 
