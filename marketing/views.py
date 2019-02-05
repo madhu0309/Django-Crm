c912e588f2c962ada55b4ef72eb1636825e325d0
Ashwin <ashwin@micropyramid.com>
2019-02-05 08:58:48 +0530

    marketing structure added

--- marketing/views.py
+++ marketing/views.py
@@ -6,28 +6,44 @@ def index(request):
 
 
 def contact_lists(request):
-    pass
+    return render(request, {}, 'marketing/lists/index.html')
 
 
-def new_contact_list(request):
-    pass
+def contact_list_new(request):
+    return render(request, {}, 'marketing/lists/new.html')
 
 
 def contact_list_detail(request):
-    pass
+    return render(request, {}, 'marketing/lists/detail.html')
 
 
 def email_template_list(request):
-    pass
+    return render(request, {}, 'marketing/email_template/index.html')
 
 
 def email_template_new(request):
-    pass
+    return render(request, {}, 'marketing/email_template/new.html')
 
 
-def campaign_open(request):
-    pass
+def email_template_edit(request):
+    return render(request, {}, 'marketing/email_template/edit.html')
 
 
-def campaign_click(request):
-    pass
+def email_template_detail(request):
+    return render(request, {}, 'marketing/email_template/detail.html')
+
+
+def campaign_list(request):
+    return render(request, {}, 'marketing/campaign/index.html')
+
+
+def campaign_new(request):
+    return render(request, {}, 'marketing/campaign/new.html')
+
+
+def campaign_edit(request):
+    return render(request, {}, 'marketing/campaign/edit.html')
+
+
+def campaign_details(request):
+    return render(request, {}, 'marketing/campaign/details.html')
