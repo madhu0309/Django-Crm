e72ba6d98d8df393cd6a332364673ec17654e9b5
vidyasagar <vidyasgar@micropyramid.com>
2019-03-06 11:38:30 +0530

    Code updated for marketing models and contact lists in it

--- marketing/models.py
+++ marketing/models.py
@@ -18,173 +18,6 @@ class Tag(models.Model):
         return self.created_by if self.created_by else None
 
 
-def attachment_url(self, filename):
-    file_extension = filename.split('.')[-1]
-    return "documents/%s" % (
-        "attachment_" + str(slugify(datetime.now())) + "." + file_extension)
-    # file_split = filename.split('.')
-    # file_extension = file_split[-1]
-    # path = "%s_%s_%s" % (file_split[0], self.profile.user.email, str(datetime.now()))
-    # return str(self.company.subdomain) + "/users/" + str(self.profile.user.email) + \
-    #     "/docs/" + slugify(path) + "." + file_extension
-
-
-class Document(models.Model):
-    title = models.CharField(max_length=155)
-    description = models.CharField(max_length=2500, blank=True, null=True)
-    document_file = models.FileField(upload_to=attachment_url)
-    created_by = models.ForeignKey(
-        User, related_name="created_marketing_documents", null=True, on_delete=models.SET_NULL)
-    created_on = models.DateTimeField(auto_now_add=True)
-    is_active = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.title
-
-    def is_document_file_image(self, ext):
-        image_ext_list = ['bmp', 'dds', 'gif', 'jpg', 'jpeg', 'png', 'psd', 'pspimage',
-                          'tga', 'thm', 'tif', 'tiff', 'yuv']
-        return ext.lower() in image_ext_list
-
-    def is_document_file_audio(self, ext):
-        audio_ext_list = ['aif', 'iff', 'm3u', 'm4a', 'mid', 'mp3',
-                          'mpa', 'wav', 'wma']
-        return ext.lower() in audio_ext_list
-
-    def is_document_file_video(self, ext):
-        video_ext_list = ['3g2', '3gp', 'asf', 'avi', 'flv', 'm4v', 'mov',
-                          'mp4', 'mpg', 'rm', 'srt', 'swf', 'vob', 'wmv']
-        return ext.lower() in video_ext_list
-
-    def is_document_file_pdf(self, ext):
-        pdf_ext_list = ['indd', 'pct', 'pdf']
-        return ext.lower() in pdf_ext_list
-
-    def is_document_file_code(self, ext):
-        code_ext_list = ['aspx', 'json', 'jsp', 'do', 'htm', 'html', 'ser',
-                         'php', 'jad', 'cfm', 'xml', 'js', 'pod', 'asp',
-                         'atomsvc', 'rdf', 'pou', 'jsf', 'abs', 'pl', 'asm',
-                         'srz', 'luac', 'cod', 'lib', 'arxml', 'bas', 'ejs',
-                         'fs', 'hbs', 's', 'ss', 'cms', 'pyc', 'vcxproj',
-                         'jse', 'smali', 'xla', 'lxk', 'pdb', 'src', 'cs',
-                         'ipb', 'ave', 'mst', 'vls', 'rcc', 'sax', 'scr',
-                         'dtd', 'axd', 'mrl', 'xsl', 'ino', 'spr', 'xsd',
-                         'cgi', 'isa', 'ws', 'rss', 'dvb', 'nupkg', 'xlm',
-                         'v4e', 'rss', 'prg', 'form', 'bat', 'mrc', 'asi',
-                         'jdp', 'fmb', 'graphml', 'gcode', 'aia', 'py', 'atp',
-                         'mzp', 'o', 'scs', 'mm', 'cpp', 'java', 'gypi', 'idb',
-                         'txml', 'c', 'vip', 'tra', 'rc', 'action', 'vlx',
-                         'asta', 'pyo', 'lua', 'gml', 'prl', 'rfs', 'cpb',
-                         'sh', 'rbf', 'gp', 'phtml', 'bp', 'scb', 'sln', 'vbp',
-                         'wbf', 'bdt', 'mac', 'rpy', 'eaf', 'mc', 'mwp', 'gnt',
-                         'h', 'swift', 'e', 'styl', 'cxx', 'as', 'liquid',
-                         'dep', 'fas', 'vbs', 'aps', 'vbe', 'lss', 'cmake',
-                         'resx', 'csb', 'dpk', 'pdml', 'txx', 'dbg', 'jsa',
-                         'sxs', 'sasf', 'pm', 'csx', 'r', 'wml', 'au3', 'stm',
-                         'cls', 'cc', 'ins', 'jsc', 'dwp', 'rpg', 'arb', 'bml',
-                         'inc', 'eld', 'sct', 'sm', 'wbt', 'csproj', 'tcz',
-                         'html5', 'gbl', 'cmd', 'dlg', 'tpl', 'rbt', 'xcp',
-                         'tpm', 'qry', 'mfa', 'ptx', 'lsp', 'pag', 'ebc',
-                         'php3', 'cob', 'csc', 'pyt', 'dwt', 'rb', 'wsdl',
-                         'lap', 'textile', 'sfx', 'x', 'a5r', 'dbp', 'pmp',
-                         'ipr', 'fwx', 'pbl', 'vbw', 'phl', 'cbl', 'pas',
-                         'mom', 'dbmdl', 'lol', 'wdl', 'ppam', 'plx', 'vb',
-                         'cgx', 'lst', 'lmp', 'vd', 'bcp', 'thtml', 'scpt',
-                         'isu', 'mrd', 'perl', 'dtx', 'f', 'wpk', 'ipf', 'ptl',
-                         'luca', 'hx', 'uvproj', 'qvs', 'vba', 'xjb',
-                         'appxupload', 'ti', 'svn-base', 'bsc', 'mak',
-                         'vcproj', 'dsd', 'ksh', 'pyw', 'bxml', 'mo', 'irc',
-                         'gcl', 'dbml', 'mlv', 'wsf', 'tcl', 'dqy', 'ssi',
-                         'pbxproj', 'bal', 'trt', 'sal', 'hkp', 'vbi', 'dob',
-                         'htc', 'p', 'ats', 'seam', 'loc', 'pli', 'rptproj',
-                         'pxml', 'pkb', 'dpr', 'scss', 'dsb', 'bb', 'vbproj',
-                         'ash', 'rml', 'nbk', 'nvi', 'lmv', 'mw', 'jl', 'dso',
-                         'cba', 'jks', 'ary', 'run', 'vps', 'clm', 'brml',
-                         'msha', 'mdp', 'tmh', 'rdf', 'jsx', 'sdl', 'ptxml',
-                         'fxl', 'wmw', 'dcr', 'bcc', 'cbp', 'bmo', 'bsv',
-                         'less', 'gss', 'ctl', 'rpyc', 'ascx', 'odc', 'wiki',
-                         'obr', 'l', 'axs', 'bpr', 'ppa', 'rpo', 'sqlproj',
-                         'smm', 'dsr', 'arq', 'din', 'jml', 'jsonp', 'ml',
-                         'rc2', 'myapp', 'cla', 'xme', 'obj', 'jsdtscope',
-                         'gyp', 'datasource', 'cp', 'rh', 'lpx', 'a2w', 'ctp',
-                         'ulp', 'nt', 'script', 'bxl', 'gs', 'xslt', 'mg',
-                         'pch', 'mhl', 'zpd', 'psm1', 'asz', 'm', 'jacl',
-                         'pym', 'rws', 'acu', 'ssq', 'wxs', 'coffee', 'ncb',
-                         'akt', 'pyx', 'zero', 'hs', 'mkb', 'tru', 'xul',
-                         'mfl', 'sca', 'sbr', 'master', 'opv', 'matlab',
-                         'sami', 'agc', 'slim', 'tea', 'pbl', 'm51', 'mec',
-                         'asc', 'gch', 'enml', 'ino', 'kst', 'jade', 'dfb',
-                         'ips', 'rgs', 'vbx', 'cspkg', 'ncx', 'brs', 'wfs',
-                         'ifp', 'nse', 'xtx', 'j', 'cx', 'ps1', 'nas', 'mk',
-                         'ccs', 'vrp', 'lnp', 'cml', 'c#', 'idl', 'exp', 'apb',
-                         'nsi', 'asmx', 'tdo', 'pjt', 'fdt', 's5d', 'mvba',
-                         'mf', 'odl', 'bzs', 'jardesc', 'tgml', 'moc', 'wxi',
-                         'cpz', 'fsx', 'jav', 'ocb', 'agi', 'tec', 'txl',
-                         'amw', 'mscr', 'dfd', 'dpd', 'pun', 'f95', 'vdproj',
-                         'xsc', 'diff', 'wxl', 'dgml', 'airi', 'kmt', 'ksc',
-                         'io', 'rbw', 'sas', 'vcp', 'resources', 'param',
-                         'cg', 'hlsl', 'vssscc', 'bgm', 'xn', 'targets', 'sl',
-                         'gsc', 'qs', 'owl', 'devpak', 'phps', 'hdf', 'pri',
-                         'nbin', 'xaml', 's4e', 'scm', 'tk', 'poc', 'uix',
-                         'clw', 'factorypath', 's43', 'awd', 'htr', 'php2',
-                         'classpath', 'pickle', 'rob', 'msil', 'ebx', 'tsq',
-                         'lml', 'f90', 'lds', 'vup', 'pbi', 'swt', 'vap', 'ig',
-                         'pdo', 'frt', 'fcg', 'c++', 'xcl', 'dfn', 'aar',
-                         'for', 're', 'twig', 'ebm', 'dhtml', 'hc', 'pro',
-                         'ahk', 'rule', 'bsh', 'jcs', 'zrx', 'wsdd', 'csp',
-                         'drc', 'appxsym']
-        return ext.lower() in code_ext_list
-
-    def is_document_file_text(self, ext):
-        text_ext_list = ['doc', 'docx', 'log', 'msg', 'odt', 'pages', 'rtf',
-                         'tex', 'txt', 'wpd', 'wps']
-        return ext.lower() in text_ext_list
-
-    def is_document_file_sheet(self, ext):
-        sheet_ext_list = ['csv', 'xls', 'xlsx',
-                          'xlsm', 'xlsb', 'xltx', 'xltm', 'xlt']
-        return ext.lower() in sheet_ext_list
-
-    def is_document_file_zip(self, ext):
-        ext_list = ['zip', '7Z', 'gz', 'rar', 'ZIPX', 'ACE', 'tar', ]
-        return ext.lower() in ext_list
-
-    def file_type(self):
-        name_ext_list = self.document_file.url.split(".")
-        if (len(name_ext_list) > 1):
-            ext = name_ext_list[int(len(name_ext_list) - 1)]
-            if self.is_document_file_audio(ext):
-                return ("audio", "fa fa-file-audio-o")
-            elif self.is_document_file_video(ext):
-                return ("video", "fa fa-file-video-o")
-            elif self.is_document_file_image(ext):
-                return ("image", "fa fa-file-image-o")
-            elif self.is_document_file_pdf(ext):
-                return ("pdf", "fa fa-file-pdf-o")
-            elif self.is_document_file_code(ext):
-                return ("code", "fa fa-file-code-o")
-            elif self.is_document_file_text(ext):
-                return ("text", "fa fa-file-text")
-            elif self.is_document_file_sheet(ext):
-                return ("sheet", "fa fa-file-excel-o")
-            elif self.is_document_file_zip(ext):
-                return ("zip", "fa fa-file-archive-o")
-            return ("file", "fa fa-file-o")
-        return ("file", "fa fa-file-o")
-
-    @property
-    def file_type_code(self):
-        return self.file_type()
-
-    @property
-    def document_file_path(self):
-        return self.document_file.url if self.document_file else None
-
-    @property
-    def document_file_name(self):
-        return self.document_file.name if self.document_file else None
-
-
 class EmailTemplate(models.Model):
     created_by = models.ForeignKey(
         User, related_name="marketing_emailtemplates", null=True, on_delete=models.SET_NULL)
@@ -192,9 +25,6 @@ class EmailTemplate(models.Model):
     title = models.CharField(max_length=5000)
     subject = models.CharField(max_length=5000)
     html = models.TextField()
-
-    class Meta:
-        unique_together = (("title", "company"),)
 
     @property
     def created_by_user(self):
@@ -207,26 +37,12 @@ class ContactList(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=500)
     tags = models.ManyToManyField(Tag)
-    is_public = models.BooleanField(default=False)
-
-    class Meta:
-        unique_together = (("name", "company"),)
+    # is_public = models.BooleanField(default=False)
+    visible_to = models.ManyToManyField(User, related_name="contact_lists_visible_to")
 
     @property
     def created_by_user(self):
         return self.created_by if self.created_by else None
-
-    @property
-    def created_by_user_profile(self):
-        if self.created_by is not None:
-            profile = User.objects.filter(
-                user=self.created_by).last()
-            if profile is not None:
-                return profile
-            else:
-                return self.created_by
-        else:
-            return None
 
     @property
     def created_on_format(self):
@@ -240,10 +56,6 @@ class ContactList(models.Model):
         if difference <= timedelta(minutes=1):
             return 'just now'
         return '%(time)s ago' % {'time': timesince(self.created_on).split(', ')[0]}
-
-    @property
-    def created_by_company(self):
-        return self.company if self.company else None
 
     @property
     def tags_data(self):
@@ -292,9 +104,6 @@ class Contact(models.Model):
     state = models.CharField(max_length=500, null=True, blank=True)
     contry = models.CharField(max_length=500, null=True, blank=True)
 
-    class Meta:
-        unique_together = (("company", "email"),)
-
 
 class Campaign(models.Model):
     STATUS_CHOICES = (
@@ -328,6 +137,25 @@ class Campaign(models.Model):
     def no_of_unsubscribers(self):
         unsubscribers = self.campaign_contacts.filter(contact__is_unsubscribed=True).count()
         return unsubscribers
+
+    @property
+    def no_of_bounces(self):
+        bounces = self.campaign_contacts.filter(contact__is_bounced=True).count()
+        return bounces
+
+    @property
+    def no_of_clicks(self):
+        clicks = self.marketing_links.aggregate(Sum('clicks'))['clicks__sum']
+        return clicks
+
+    @property
+    def no_of_sent_emails(self):
+        contacts = self.campaign_contacts.count()
+        return contacts
+
+    @property
+    def created_on_format(self):
+        return self.created_on.strftime('%b %d, %Y %I:%M %p')
 
 
 class Link(models.Model):
--- marketing/views.py
+++ marketing/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+import json
+from django.conf import Settings
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
+from django.http.response import JsonResponse, HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy, reverse
+from common import status
+from marketing.models import Tag, ContactList, Contact
+from marketing.forms import ContactListForm
+from marketing.tasks import upload_csv_file
+
+
+def get_exact_match(query, m2m_field, ids):
+    # query = tasks_list.annotate(count=Count(m2m_field))\
+    #             .filter(count=len(ids))
+    query = query
+    for _id in ids:
+        query = query.filter(**{m2m_field: _id})
+    return query
 
 
 def dashboard(request):
@@ -6,7 +25,38 @@ def dashboard(request):
 
 
 def contact_lists(request):
-    return render(request, 'marketing/lists/index.html')
+    tags = Tag.objects.all()
+    if (request.user.is_superuser):
+        queryset = ContactList.objects.all()
+    else:
+        queryset = ContactList.objects.filter(created_by=request.user)
+    if request.method == 'POST':
+        post_tags = request.POST.get('tags')
+        if request.POST.get('search'):
+            queryset = queryset.filter(
+                Q(name__icontains=request.POST['search']) |
+                Q(created_by__email__icontains=request.POST['search'])).distinct()
+
+        if post_tags:
+
+            filtered_list = json.loads(request.POST['tags'])
+
+            if len(filtered_list) > 1:
+                queryset = get_exact_match(queryset, 'tags', filtered_list)
+            else:
+                queryset = queryset.filter(tags__id__in=filtered_list)
+
+    per_page = request.GET.get("per_page", 10)
+    paginator = Paginator(queryset, per_page)
+    page = request.GET.get('page', 1)
+    try:
+        contact_lists = paginator.page(page)
+    except PageNotAnInteger:
+        contact_lists = paginator.page(1)
+    except EmptyPage:
+        contact_lists = paginator.page(paginator.num_pages)
+    data = {'contact_lists': contact_lists, 'tags': tags}
+    return render(request, 'marketing/lists/index.html', data)
 
 
 def contacts_list(request):
@@ -14,7 +64,87 @@ def contacts_list(request):
 
 
 def contact_list_new(request):
-    return render(request, 'marketing/lists/new.html')
+    data = {}
+    if request.POST:
+        form = ContactListForm(request.POST, request.FILES)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.created_by = request.user
+            instance.save()
+            for each in json.loads(request.POST['tags']):
+                tag, _ = Tag.objects.get_or_create(
+                    name=each, created_by=request.user)
+                instance.tags.add(tag)
+            if request.FILES.get('contacts_file'):
+                upload_csv_file.delay(form.validated_rows, request.user.id, [instance.id])
+
+            return JsonResponse({'error': False, 'data': form.data}, status=status.HTTP_201_CREATED)
+        return JsonResponse({'error': True, 'errors': form.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+    return render(request, 'marketing/lists/new.html', data)
+
+
+def edit_contact_list(request, pk):
+    user = request.user
+    try:
+        contact_list = ContactList.objects.get(pk=pk)
+    except ContactList.DoesNotExist:
+        contact_list = ContactList.objects.none()
+        return JsonResponse({}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        # contact_lists = ContactList.objects.filter(company=request.company)
+        if (user.is_superuser):
+            contact_lists = ContactList.objects.all()
+        else:
+            contact_lists = ContactList.objects.filter(created_by=request.user)
+
+        data = {'contact_list': contact_list, 'contact_lists': contact_lists}
+        return render(request, 'marketing/lists/new.html', data)
+    else:
+        form = ContactListForm(request.POST, request.FILES, instance=contact_list)
+        if form.is_valid():
+            instance = form.save()
+            instance.tags.clear()
+            for each in json.loads(request.POST['tags']):
+                tag, _ = Tag.objects.get_or_create(name=each, created_by=request.user)
+                instance.tags.add(tag)
+            if request.FILES.get('contacts_file'):
+                upload_csv_file.delay(form.validated_rows, request.user.id, [instance.id])
+
+            return JsonResponse(form.data, status=status.HTTP_201_CREATED)
+        return JsonResponse({'error': True, 'errors': form.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
+def view_contact_list(request, pk):
+    contact_list = get_object_or_404(ContactList, pk=pk)
+    contacts = Contact.objects.filter(contact_list__in=[contact_list])
+    if request.POST and request.POST.get("search"):
+        contacts = contacts.filter(
+            Q(name__icontains=request.POST['search']) | Q(email__icontains=request.POST['search']))
+
+    per_page = request.GET.get("per_page", 10)
+    paginator = Paginator(contacts.order_by('id'), per_page)
+    page = request.GET.get('page', 1)
+    try:
+        contacts = paginator.page(page)
+    except PageNotAnInteger:
+        contacts = paginator.page(1)
+    except EmptyPage:
+        contacts = paginator.page(paginator.num_pages)
+
+    data = {'contact_list': contact_list, "contacts": contacts}
+    template_name = "marketing/lists/all.html"
+    return render(request, template_name, data)
+
+
+def delete_contact_list(request, pk):
+    try:
+        ContactList.objects.get(pk=pk).delete()
+        redirect_to = HttpResponseRedirect(reverse('marketing:contact_lists'))
+    except ContactList.DoesNotExist:
+        redirect_to = HttpResponseRedirect(reverse('marketing:contact_lists'))
+    return HttpResponseRedirect(redirect_to)
 
 
 def contacts_list_new(request):
