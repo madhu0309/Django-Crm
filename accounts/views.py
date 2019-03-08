b2be2f3ecbbda387e3dc0facfe40ce6c41e259ba
madhuri <madhuri@micropyramid.com>
2019-03-08 12:38:07 +0530

    #189 issue fixed

--- accounts/views.py
+++ accounts/views.py
@@ -156,7 +156,8 @@ class CreateAccountView(LoginRequiredMixin, CreateView):
         context["industries"] = INDCHOICES
         context["countries"] = COUNTRIES
         context["contact_count"] = Contact.objects.count()
-        context["lead_count"] = Lead.objects.exclude(status='dead').count()
+        context["leads"] = Lead.objects.exclude(status='converted' and 'dead')
+        context["lead_count"] = context["leads"].count()
         if self.request.user.role != "ADMIN" and not self.request.user.is_superuser:
             context["lead_count"] = Lead.objects.filter(
                 Q(assigned_to__in=[self.request.user]) | Q(created_by=self.request.user)).exclude(status='dead').count()
@@ -275,7 +276,8 @@ class AccountUpdateView(LoginRequiredMixin, UpdateView):
         context["industries"] = INDCHOICES
         context["countries"] = COUNTRIES
         context["contact_count"] = Contact.objects.count()
-        context["lead_count"] = Lead.objects.exclude(status='dead').count()
+        context["leads"] = Lead.objects.exclude(status='converted' and 'dead')
+        context["lead_count"] = context["leads"].count()
         if self.request.user.role != "ADMIN" and not self.request.user.is_superuser:
             context["lead_count"] = Lead.objects.filter(
                 Q(assigned_to__in=[self.request.user]) | Q(created_by=self.request.user)).exclude(status='dead').count()
