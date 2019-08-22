ef2935b1f7561dd623ea972b2e195abc2033d006
sreevardhan <sreevardhan@micropyramid.com>
2019-08-22 21:51:31 +0530

    added celery test cases

--- invoices/tests_celery_tasks.py
+++ invoices/tests_celery_tasks.py
@@ -1,23 +1,22 @@
-from datetime import datetime, timedelta
-
-from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 from django.test.utils import override_settings
-from django.urls import reverse
 
-from accounts.models import Account
-from common.models import Address, Attachments, Comment, User
-from invoices.models import Invoice, InvoiceHistory
-from invoices.tasks import send_email
+from invoices.tasks import (send_email, send_invoice_email,
+                            send_invoice_email_cancel)
 from invoices.tests import InvoiceCreateTest
-from teams.models import Teams
 
 
-# class AddTestCase(InvoiceCreateTest, TestCase):
+class TestSendMailOnInvoiceCreationTask(InvoiceCreateTest, TestCase):
 
-#     @override_settings(CELERY_EAGER_PROPAGATES_EXCEPTIONS=True,
-#                        CELERY_ALWAYS_EAGER=True,
-#                        BROKER_BACKEND='memory')
-#     def test_mytask(self):
-#         task = send_email.apply((self.invoice.id,))
-#         self.assertEqual('SUCCESS', task.state)
+    @override_settings(CELERY_EAGER_PROPAGATES_EXCEPTIONS=True,
+                       CELERY_ALWAYS_EAGER=True,
+                       BROKER_BACKEND='memory')
+    def test_send_mail_on_invoice_creation_task(self):
+        task = send_email.apply((self.invoice.id,))
+        self.assertEqual('SUCCESS', task.state)
+
+        task = send_invoice_email.apply((self.invoice.id,))
+        self.assertEqual('SUCCESS', task.state)
+
+        task = send_invoice_email_cancel.apply((self.invoice.id,))
+        self.assertEqual('SUCCESS', task.state)
