8cb3f98d78a800c97a60eae922f41fbbfa0e1d64
sreevardhan <sreevardhan@micropyramid.com>
2019-09-20 20:12:46 +0530

    added cache for images in leads list page

--- crm/settings.py
+++ crm/settings.py
@@ -354,9 +354,9 @@ else:
     print("No local settings file found")
 
 
-# CACHES = {
-#     'default': {
-#         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-#         'LOCATION': '127.0.0.1:11211',
-#     }
-# }
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
