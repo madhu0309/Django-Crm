d08909861ca8d45dc678ef89951f428bbfe119d4
teja.muvva <teja@micropyramid.com>
2019-10-11 14:20:12 +0530

    minor change in leads filter

--- leads/views.py
+++ leads/views.py
@@ -94,10 +94,12 @@ def lead_list_view(request):
 
         request_post = request.POST
         if request_post:
+            print("11:::",request_post.get('name'))
             if request_post.get('name'):
                 queryset = queryset.filter(
-                    Q(first_name__icontains=request_post.get('name')) &
+                    Q(first_name__icontains=request_post.get('name')) or
                     Q(last_name__icontains=request_post.get('name')))
+                print("2222::",queryset)
             if request_post.get('city'):
                 queryset = queryset.filter(
                     city__icontains=request_post.get('city'))
